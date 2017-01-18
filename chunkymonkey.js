52e41018a7d42b775e50ef7531786a96943644e2
dr jules <drjules@anniecannons.com>
2017-01-17 16:17:21 -0800

    solved chunkaMonka!

--- chunkymonkey.js
+++ chunkymonkey.js
@@ -15,9 +15,23 @@
 //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 
 
+//Input: an array and a number that tells us how to slpit the array
+//Output: a two-dimensional array[[]]
+// create a variable to hold the child arrays
+  //looping through the array in order to slice and push the elements into new arrays
+
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  return arr;
+    var parentArr = [];
+    for(var i = 0; i < arr.length; i+= size) {
+      var child = arr.slice(i, size + i);
+      parentArr.push(child);
+    }
+  console.log(parentArr);
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups(["a", "b", "c", "d"], 2); //should return [["a", "b"], ["c", "d"]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3); //should return [[0, 1, 2], [3, 4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2); //should return [[0, 1], [2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4); //should return [[0, 1, 2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3); //should return [[0, 1, 2], [3, 4, 5], [6]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4); //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
