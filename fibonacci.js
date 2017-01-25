555fb05809d301bcc337d1e7d3d1dc0c6056128b
dr jules <drjules@anniecannons.com>
2017-01-24 16:23:41 -0800

    Fibonacci solved

--- fibonacci.js
+++ fibonacci.js
@@ -6,16 +6,36 @@
 
 //As an example, passing 4 to the function should return 5 because all the odd Fibonacci numbers under 4 are 1, 1, and 3.
 
+//Input: any positive integer
+//Ouput: a number (the sum of all odd Fibonacci numbers leading upt to our input number)
+
+//use a loop to go through all of the numbers up to that input number
+//create the sequence
+//use a conditional statement and modulo to see if the Fibonacci number is odd
+//if it is odd, we will add that to our new variable
+//return the result
 
 function sumFibonacci(num) {
-  //your code here
+  var previousNum = 0;
+  var currentNum = 1;
+  var result =0;
+  
+  while (currentNum <= num){
+    if (currentNum % 2 !== 0){
+      result += currentNum;
+    }
+   var sum = currentNum + previousNum;
+    previousNum = currentNum;
+    currentNum = sum;
+  }
+    
+   console.log(result); 
 }
 
 
 
 //TEST CASES
-//sumFibonacci(1000); should return 1785.
-//sumFibonacci(4000000); should return 4613732.
-//sumFibonacci(4); should return 5.
-//sumFibonacci(75024); should return 60696.
-//sumFibonacci(75025); should return 135721.
+sumFibonacci(1000); //should return 1785.
+sumFibonacci(4000000); //should return 4613732.
+sumFibonacci(4); //should return 5.
+sumFibonacci(75024); //should return 60696.
