2c4a120888977fb630bb4678487467ad75f98a81
dr jules <drjules@anniecannons.com>
2017-01-30 14:05:11 -0800

    sorted solved

--- sortedUnited.js
+++ sortedUnited.js
@@ -8,14 +8,42 @@
 //Here are some helpful links:
 
 //Array.reduce()  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+//Input: Two or more arrays
+//Output: a new array in original order with no duplicates
+
+//Create a new array
+//create a var to hold our arguments array
+//loop through the length of arguments to 
+//Loop through the individual elements
+//Compare the values of competing repeating numbers, use indexOf to search our array to see if the number exists, if it does not exist, do nothing, if it doesn't, push it
+//push the numbers that match the requirements into that array
+//return that array
 
 function uniteUnique(arr) {
-  //your code here
+  var newArray = [];
+  
+  for (var i = 0; i < arguments.length; i++){
+    var numbers = arguments[i];
+    for (var j = 0; j < numbers.length; j++){
+      if(newArray.indexOf(numbers[j]) === -1){
+        newArray.push(numbers[j]);
+      }
+    }
+    
+  }
+  return newArray;
 }
 
 
+
+
+
+
 //TEST CASES
-// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
-// uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
-// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
-// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+
+
+var result = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
+console.log(result);
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) //should return [1, 3, 2, [5], [4]].
+uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) //should return [1, 2, //3, 5, 4, 6, 7, 8].
